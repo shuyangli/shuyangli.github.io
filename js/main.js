ca82df92ef4eb9487fdc7c3b565009ad6ec4811b
Shuyang Li <shuyang.li.95@gmail.com>
2014-06-18 21:54:13 -0700

    added recommendation form

--- js/main.js
+++ js/main.js
@@ -1,14 +1,71 @@
-// resize landing div so that it's always the size of the window
+// Resize landing div so that it's always the size of the window
 function resizeLandingContent() {
 	var e = $(window).height() * 2 / 3;
 	$('#landing').css("height", e + "px")
 }
 
+// Introduce recommendation form at "let me know" click
+function introduceRecForm() {
+	if (!$("#recFormContainer").has("#recForm").length) {
+		var formHTMLString = "<form id=\"recForm\" class=\"sl-form col-sm-offset-1\">\
+			<label>your name: <input type=\"text\" name=\"friendName\" id=\"friendName\" /></label><br />\
+			<label>link or title of your recommendation: <input type=\"text\" name=\"recDetail\" id=\"recDetail\" /></label><br />\
+			<label>how can i contact you: <input type=\"text\" name=\"friendContact\" id=\"friendContact\" /></label><br />\
+			<a id=\"submitRecFormButton\" role=\"button\" onClick=\"submitRecForm()\">submit</span>\
+		</form>";
+		$(formHTMLString).hide().appendTo("#recFormContainer").fadeIn(300);
+	} else {
+		$("#recForm").fadeOut(300, function(){$(this).remove()});
+	}
+}
+
+// Submit recommendation form to Parse
+function submitRecForm() {
+
+	// Send
+	if ($('#recDetail').val().length > 0) {
+
+		var Recommendation = Parse.Object.extend("Recommendation");
+		var newRec = new Recommendation();
+
+		newRec.save({
+						friendName: $('#friendName').val(), 
+						recDetail: $('#recDetail').val(),
+						friendContact: $('#friendContact').val()
+					})
+		.then(function(newRec) {
+			// Remove onClick on #submitRecFormButton
+			$("#submitRecFormButton").attr('onclick', '');
+
+			// Display a "thanks" label
+			$("<span>thank you</span>").hide().appendTo('#recForm').fadeIn(300);
+			$("#recForm").delay(1500).fadeOut(300, function() {$(this).remove()});
+		});
+	} else {
+		// recommendation field is empty
+	}
+}
+
 $(document).ready(function() {
+
+	// Set up laning element resize
 	resizeLandingContent();
 
-	// bind the function to window resizing function
+	// Bind the resize function to window resizing function
 	$(window).resize(function() {
 		resizeLandingContent();
 	})
+
+
+	// Resize the text input
+	// $(".sl-form").find("label").find("input").keydown(function() {
+	// 	var len = $(this).val().length;
+	// 	if (len > 3) {
+	// 		// increase width
+	// 		$(this).width(len * 11);
+	// 	} else {
+	// 		// restore minimal width;
+	// 		$(this).width(50);
+	// 	}
+	// });
 });
