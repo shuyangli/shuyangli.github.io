689b56e2d01cceeee89c0bd905d29c3994298136
Shuyang Li <shuyang.li.95@gmail.com>
2014-10-25 15:41:53 -0400

    Minor changes to jquery

--- project/vcd7-starbucks/js/app.js
+++ project/vcd7-starbucks/js/app.js
@@ -46,17 +46,15 @@ $(document).ready(function () {
 
 	// Replace all @2x images
 	if (window.devicePixelRatio == 2) {
-		var images = $("img.js-2x-img");
-		for (var i = 0; i < images.length; ++i) {
-			
+		$("img.js-2x-img").each(function (index, image) {
 			// Append "@2x" into image name
-			var imageType = images[i].src.substr(-4);
-			var imageName = images[i].src.substr(0, images[i].src.length - 4);
+			var imageType = image.src.substr(-4);
+			var imageName = image.src.substr(0, image.src.length - 4);
 			imageName += "@2x" + imageType;
 
 			// Reassign image source
-			images[i].src = imageName;
-		}
+			image.src = imageName;
+		});
 	}
 
 	// Display infobox contents, and schedule infobox refresh
