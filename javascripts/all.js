5cb31b61814354e6e791d24885bebe56de8f8bfa
Shuyang Li <shuyang.li.95@gmail.com>
2019-02-13 17:26:05 +0800

    Site updated to 7ea10a1

--- javascripts/all.js
+++ javascripts/all.js
@@ -4,12 +4,6 @@ $(document).ready(function () {
 
   $("#_email_link").prop("href", "mailto:" + window._deobfuscate_email(obfuscated_email));
 
-  // Toggle footer
-  $("#_quote_block_toggle").on("click", function() {
-    $("#_quote_block").fadeToggle(400);
-  });
-  $("#_quote_block").hide();
-
   // Setup page-specific calls
   if (typeof window._page_setup !== 'undefined') {
     window._page_setup();
